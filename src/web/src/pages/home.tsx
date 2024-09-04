727cc8cef67da2a752763220b8b297da9ccae00c
KalebIsaias <kiscstudies@gmail.com>
2024-09-04 14:39:36 -0300

    feat: KKKKKKKKKKKKKKKKKKKKKKKKKKKKK

---
+++ src/web/src/pages/home.tsx
@@ -0,0 +1,7 @@
+export function Home(){
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl text-center text-white-400">Bem vindo!</h1>
+    </div>
+  ) 
+}
